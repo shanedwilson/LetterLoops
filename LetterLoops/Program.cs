110055c12ba8bd9de8d5096358ebc4570d7e8a86
Shane D Wilson <me@shanedwilson.com>
2019-02-28 07:57:11 -0600

    working

--- LetterLoops/Program.cs
+++ LetterLoops/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
 
 namespace LetterLoops
 {
@@ -6,7 +9,30 @@ namespace LetterLoops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a string of letters.");
+            var userInput = Console.ReadLine();
+            var userOutput = "";
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    if (j == 0)
+                    {
+                        userOutput += userInput[i].ToString().ToUpper();
+                    }
+                    else
+                    {
+                        userOutput += userInput[i].ToString().ToLower();
+                    }
+                }
+                if (userInput[i] != userInput.Length - 1)
+                {
+                    userOutput += "-";
+                }
+        }
+            Console.WriteLine(userOutput);
+            Console.ReadKey();
         }
     }
 }
